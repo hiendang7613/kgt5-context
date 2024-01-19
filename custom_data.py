1baa0bc79c05bf52c97a549d93f3c5d72ad38d52
Hiendang7613 <73353492+hiendang7613@users.noreply.github.com>
2024-01-19 15:15:40 +0700

    Update custom_data.py

--- custom_data.py
+++ custom_data.py
@@ -59,10 +59,10 @@ class NbhoodDataModule(pl.LightningDataModule):
         input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True)
         labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True)
         labels[labels == 0] = -100
-        attention_mask = (input_ids != 0)
         #.to(device)
         if input_ids.size(1) < self.max_input_sequence_length:
           input_ids = F.pad(input_ids, (0, self.max_input_sequence_length - input_ids.size(1)))
+        attention_mask = (input_ids != 0)
 
         return {
             "input_ids": input_ids,
